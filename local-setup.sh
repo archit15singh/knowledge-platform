5f2f8ceea97be3a84e46963e70e0db086c40c72c
Archit Singh <131599737+archit15singh@users.noreply.github.com>
2023-11-02 21:55:55 +0000

    update: add detach flag to compose

--- local-setup.sh
+++ local-setup.sh
@@ -3,4 +3,4 @@
 mkdir -p ~/sunbird-dbs/neo4j ~/sunbird-dbs/cassandra ~/sunbird-dbs/redis ~/sunbird-dbs/es ~/sunbird-dbs/kafka
 export sunbird_dbs_path=~/sunbird-dbs
 
-docker compose up
+docker compose up -d
